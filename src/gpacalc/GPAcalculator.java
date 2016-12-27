7d4dff5744d1baa0b5471e6923c7f7c3bdd793db
Bryan Martinez <martineb1@hawkmail.newpaltz.edu>
2016-12-27 18:33:27 -0500

    Modified license info

--- src/gpacalc/GPAcalculator.java
+++ src/gpacalc/GPAcalculator.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2016 Bryan R. Martinez (https://github.com/bryanrm)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package gpacalc;
 
@@ -17,9 +28,11 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 
 /**
- *
- * @author Bryan Ruben Martinez
- * @version 2.0
+ * This program calculates a students GPA given course grade and credits worth.
+ * Originally written in 2015.
+ * 
+ * @author Bryan R. Martinez
+ * @version 1.0
  */
 public class GPAcalculator extends javax.swing.JFrame {
 
